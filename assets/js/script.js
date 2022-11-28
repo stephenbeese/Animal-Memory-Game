eb12e6099d919aa098e12df9c256a172b49eec8f
stephenbeese <stephenbeese1@live.co.uk>
2022-11-28 19:52:02 +0000

    Add call for check match function when two cards have been picked

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,18 +49,24 @@ let cards = [
         img: 'assets/images/sheep.png',
     }
 ]
+
+let cardsPicked = []; // empty array for picked cards
 /**
  * This function reveals the image of the card clicked
  */
-function cardPicked() {    
-    alert("You have clicked a card");
+function cardPicked() {
     let cardId = this.getAttribute('id'); // gets id attribute
-    let name = this.getAttribute('class');
-
-    console.log(cardId);
-    console.log(name);
-    
+    let name = this.getAttribute('class'); // gets class attribute
     document.getElementById(cardId).style.opacity = "1"; // changes image clicked opacity to 1 
+    // console.log(name);
+
+    cardsPicked.push(name); // pushes class name into cardsPicked array.
+
+    console.log(cardsPicked);
+
+    if (cardsPicked.length === 2) {
+        setTimeout(checkMatch, 500);
+    }
 
 }
 
@@ -79,7 +85,7 @@ function createGameBoard() {
 
         card.setAttribute('src', animalImg); // sets the image source to img element
         card.classList.add(animalName); // adds image class and class of specific animal name
-        card.setAttribute('id', [i]); // 
+        card.setAttribute('id', [i]); // sets id attribut to index number
 
         card.addEventListener('click', cardPicked); // adds event listener to call cardPicked function 
 
@@ -87,13 +93,17 @@ function createGameBoard() {
     }
 }
 
-createGameBoard();
+function checkMatch() {
+    alert('Checking for match');
+    console.log("check for match")
+}
 
+createGameBoard();
 
 
 // function runGame()
 
-// function checkMatch()
+
 
 // function cardPicked() {
 //     alert("You have clicked a card")
