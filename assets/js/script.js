16082481fccf317389a0d9b87b406ca12e9283ce
stephenbeese <stephenbeese1@live.co.uk>
2022-11-28 11:32:51 +0000

    Add card names and images to an array

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// Array for card images
+let cards = [
+    {
+        name: 'cat',
+        img: 'assets/images/cat.jpeg',
+    },
+    {
+        name: 'chicken',
+        img: 'assets/images/chicken.jpeg',
+    },
+    {
+        name: 'cow',
+        img: 'assets/images/cow.jpeg',
+    },
+    {
+        name: 'duck',
+        img: 'assets/images/duck.jpeg',
+    },
+    {
+        name: 'pig',
+        img: 'assets/images/pig.jpeg',
+    },
+    {
+        name: 'sheep',
+        img: 'assets/images/sheep.jpeg',
+    },
+    {
+        name: 'cat',
+        img: 'assets/images/cat.jpeg',
+    },
+    {
+        name: 'chicken',
+        img: 'assets/images/chicken.jpeg',
+    },
+    {
+        name: 'cow',
+        img: 'assets/images/cow.jpeg',
+    },
+    {
+        name: 'duck',
+        img: 'assets/images/duck.jpeg',
+    },
+    {
+        name: 'pig',
+        img: 'assets/images/pig.jpeg',
+    },
+    {
+        name: 'sheep',
+        img: 'assets/images/sheep.jpeg',
+    }
+]
+
