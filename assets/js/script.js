c2205be2a602abb4e91c0cd057c8835ab263e028
stephenbeese <stephenbeese1@live.co.uk>
2022-11-28 15:42:09 +0000

    Replace images to include transparent background, add some styling to new images and add animal name class to image with javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,68 +2,71 @@
 let cards = [
     {
         name: 'cat',
-        img: 'assets/images/cat.jpeg',
+        img: 'assets/images/cat.png',
     },
     {
         name: 'chicken',
-        img: 'assets/images/chicken.jpeg',
+        img: 'assets/images/chicken.png',
     },
     {
         name: 'cow',
-        img: 'assets/images/cow.jpeg',
+        img: 'assets/images/cow.png',
     },
     {
         name: 'duck',
-        img: 'assets/images/duck.jpeg',
+        img: 'assets/images/duck.png',
     },
     {
         name: 'pig',
-        img: 'assets/images/pig.jpeg',
+        img: 'assets/images/pig.png',
     },
     {
         name: 'sheep',
-        img: 'assets/images/sheep.jpeg',
+        img: 'assets/images/sheep.png',
     },
     {
         name: 'cat',
-        img: 'assets/images/cat.jpeg',
+        img: 'assets/images/cat.png',
     },
     {
         name: 'chicken',
-        img: 'assets/images/chicken.jpeg',
+        img: 'assets/images/chicken.png',
     },
     {
         name: 'cow',
-        img: 'assets/images/cow.jpeg',
+        img: 'assets/images/cow.png',
     },
     {
         name: 'duck',
-        img: 'assets/images/duck.jpeg',
+        img: 'assets/images/duck.png',
     },
     {
         name: 'pig',
-        img: 'assets/images/pig.jpeg',
+        img: 'assets/images/pig.png',
     },
     {
         name: 'sheep',
-        img: 'assets/images/sheep.jpeg',
+        img: 'assets/images/sheep.png',
     }
 ]
 
 // Randomly sorts cards and pushes them into divs with class "img-div"
 function createGameBoard() {
-    cards.sort(() => Math.random() - 0.5);
+    cards.sort(() => Math.random() - 0.5); // randomly sorts cards array
 
     let images = document.getElementsByClassName("img-div");
     let card = document.createElement('img');
 
     for (i = 0; i < cards.length; i++) {
-        let card = document.createElement('img');
-        animalImg = cards[i].img
-        card.setAttribute('src', animalImg)
-        card.setAttribute('class', 'image')
-        images[i].appendChild(card);
-        console.log(card, i)
+        let card = document.createElement('img'); // creates img element
+
+        let animalImg = cards[i].img; // picks out the image file path
+        let animalName = cards[i].name; // picks out the card name
+
+        card.setAttribute('src', animalImg); // sets the image source to img element
+        card.classList.add('image', animalName); // adds image class and class of specific animal name
+
+        images[i].appendChild(card); // adds images within "img-div" class
     }
 }
 
